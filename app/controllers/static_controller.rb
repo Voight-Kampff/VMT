3cd9c1fbd4ae3f67ab56bb93e827bd190a15dd75
Voight Kampff <valfells@gmail.com>
2012-11-11 23:20:35 +0100

    first commit

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,36 @@
+class StaticController < ApplicationController
+
+	def home
+	end
+
+	def programme
+	end
+
+	def presse
+	end
+
+	def association
+	end
+
+	def contact
+	end
+
+	def status
+	end
+
+	def arthus
+	end
+
+	def ophelie
+	end
+
+	def intermezzo
+	end
+	
+	def nemanja
+	end
+
+	def khatia
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VMT::Application.config.secret_token = '74725567c615698eb9535d66ca7aecdaf03c774a188f6d2d685b18b2dda8bb6fe4290001cce07dc54f2e4dc34041e9447c036b80e96c0161a280b863bb3c8667'
