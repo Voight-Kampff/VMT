b15a93b2e45a9be1817721bf6888186c5416a6ad
Voight-Kampff <valfells@gmail.com>
2013-05-06 17:37:37 +0200

    Last attempd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ VMT::Application.configure do
       :port                 => 587,
       :domain               => 'musicales-tannay.ch',
       :user_name            => 'billetterie@musicales-tannay.ch',
-      :password             => BILLETTERIE_PASSWORD,
+      :password             => "Kttk13vw",
       :authentication       => :plain,
       :enable_starttls_auto => true  }
 
