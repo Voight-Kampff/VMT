4ccae382cfae7b7d3e31753b70b86b6d340a783e
Voight-Kampff <valfells@gmail.com>
2013-06-09 17:23:02 +0200

    Fixed Typo

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Order do
 	batch_action :paid do |selection|
       Order.find(selection).each do |order|
         order.paid = true
-        order.true
+        order.save
       end
     end
   
