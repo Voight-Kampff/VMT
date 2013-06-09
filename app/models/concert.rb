435a4030a1b954bd2bdfff6b0e2b552cd2f19a82
Voight-Kampff <valfells@gmail.com>
2013-06-09 19:07:39 +0200

    Fixed artist info

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,7 +1,7 @@
 class Concert < ActiveRecord::Base
   default_scope order('date ASC')
   serialize :playlist, Array
-  attr_accessible :date, :description, :head, :subhead, :shortname, :playlist, :price
+  attr_accessible :date, :description, :head, :subhead, :shortname, :playlist, :price, :category
   has_many :artists
   has_many :items
 
