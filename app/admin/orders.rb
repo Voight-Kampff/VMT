7a55d08c602e149ffcd47c9a77846ba127be61d1
Voight-Kampff <valfells@gmail.com>
2013-06-09 17:19:31 +0200

    Added Active Admin Batch Action

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register Order do
+
+	batch_action :paid do |selection|
+      Order.find(selection).each do |order|
+        order.paid = true
+        order.true
+      end
+    end
   
 end
