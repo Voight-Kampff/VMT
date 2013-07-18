cb461a75829b5abb712d0702ea3598142fdc697f
Voight-Kampff <valfells@gmail.com>
2013-07-18 14:44:41 +0200

    Active Admin View change and redirect
    Fixed Redirect to Prs view
    Added Tickets sub column to ActiveAdmin Index

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -8,5 +8,78 @@ ActiveAdmin.register Order do
         redirect_to "/admin/orders"
       end
     end
+
+
+    index do
+    	selectable_column
+    	id_column
+    	column("Nom", :name)
+    	column :email
+    	column("Rue", :street)
+    	column :NPA
+    	column :Ville
+    	column :total do |order|
+    		"#{order.total/100} CHF"
+    	end
+    	column :transfer
+    	column :paid
+    	column :billets do |order|
+    		table_for order.tickets do
+     			column :concert do |ticket|
+        		link_to ticket.concert.shortname
+      			end
+      			column :normal
+      			column :student
+    		end
+  		end
+  		column :membership_id do |ticket|
+  			ticket.membership.name unless ticket.membership.nil?
+  		end
+    	column :created_at
+    	default_actions
+    end
+
+   #  index :as => :block do |order|
+   #  	div :for => order do
+   #    		h2 auto_link(order.name)
+   #    		div do
+   #      	simple_format order.street
+   #    		end
+   # 		end
+  	# end
+
+  csv do
+    	column :name
+    	column :email
+    	column :street
+    	column :Ville
+    	column :NPA
+    	column :total
+    	column :transfer
+    	column :paid
+    	column :billets do |order|
+    		table_for order.tickets do
+     			column :concert do |ticket|
+        		link_to ticket.concert.shortname
+      			end
+      			column :normal
+      			column :student
+    		end
+  		end
+  		column :membership_id do |ticket|
+  			ticket.membership.name unless ticket.membership.nil?
+  		end
+    	column :created_at
+    end
+
+    show :title => :name do
+    	panel "Billets" do
+    		table_for(order.tickets) do
+    			column("concert",:sortable => :id) {|ticket| link_to "##{ticket.concert.shortname}", admin_ticket_path(ticket) }
+    		end
+    	end
+    	active_admin_comments
+    end
+
   
 end
