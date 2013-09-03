d83e7051dc574c0bac11eef2fa3be4d58c29887f
Voight-Kampff <valfells@gmail.com>
2013-09-03 22:15:28 +0200

    Programme

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -5,7 +5,7 @@ class ConcertsController < ApplicationController
   end
 
   def index
-  	@concerts=Concert.all #where(["date > ? AND category = ?", Time.now, 'concert'])
+  	@concerts=Concert.where(["date > ? AND category = ?", 2013, 'concert'])
   	@abonnement=Concert.where(["date > ? AND category = ?", Time.now, 'abonnement'])
   end
 
