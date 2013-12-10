2e665f9d3f0f8ddcdfe80d4c529b7bad571ba089
Voight-Kampff <valfells@gmail.com>
2013-12-10 18:33:02 -0500

    fixed path problem

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,4 +2,4 @@
 
 Paperclip::Attachment.default_options[:url] = 'variations.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:s3_host_alias] = 'photos.musicales-tannay.ch'
-Paperclip::Attachment.default_options[:path] = '/photos/#{self.gallery}/:style/:filename'
+Paperclip::Attachment.default_options[:path] = '/photos/:style/:filename'
