4c51555880b628aa431fe260b820801cfa5bd71d
Voight-Kampff <valfells@gmail.com>
2013-12-10 22:41:32 -0500

    try again

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,9 +1,9 @@
 #options for paperclip
 
-#Paperclip::Attachment.default_options[:url] = 'variations.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = 'variations.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:s3_domain_url] = 's3_alias_url'
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
-Paperclip::Attachment.default_options[:s3_alias_url] = 'photos.musicales-tannay.ch'
+Paperclip::Attachment.default_options[:s3_host_name] = 'photos.musicales-tannay.ch'
+#Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
 Paperclip::Attachment.default_options[:s3_host_alias] = 'photos.musicales-tannay.ch'
 Paperclip::Attachment.default_options[:path] = '/photos/:style/:filename'
 Paperclip::Attachment.default_options[:use_timestamp] = false
