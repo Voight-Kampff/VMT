0d183b43bb22e67bb1893244e2adfa8bb87d3a88
Voight-Kampff <valfells@gmail.com>
2013-12-10 22:18:58 -0500

    Last commit didn't work

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,7 +2,7 @@
 
 #Paperclip::Attachment.default_options[:url] = 'variations.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:url] = 's3_alias_url'
-#Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
 Paperclip::Attachment.default_options[:s3_host_alias] = 'photos.musicales-tannay.ch'
 Paperclip::Attachment.default_options[:path] = '/photos/:style/:filename'
 Paperclip::Attachment.default_options[:use_timestamp] = false
