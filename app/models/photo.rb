26a39675aa424d8c0afc2ce1dd636bdcd4c9ab8d
Voight-Kampff <valfells@gmail.com>
2013-12-10 23:38:59 -0500

    Removed Interpolation attempt

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,14 +3,9 @@ class Photo < ActiveRecord::Base
 	attr_accessible :image
 	attr_accessible :gallery
 
-	Paperclip.interpolates :gallery do |attachement, style|
-		attachment.instance.self.gallery
-	end
-
 	has_attached_file :image, styles: {
 		thumb: '158x110'
 	},
-	:path => '/photos/:gallery/:style/:filename',
 	:url => ':s3_alias_url',
     :s3_host_alias => 'photos.musicales-tannay.ch'
 end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
 #options for paperclip
 
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/photos/:style/:filename'
+Paperclip::Attachment.default_options[:path] = '/:class/:style/:filename'
 Paperclip::Attachment.default_options[:use_timestamp] = false
