c76bbe2c22e4c4d37a9d97994c3ea41e69f0796f
Voight-Kampff <valfells@gmail.com>
2013-12-10 21:38:57 -0500

    Fixed Path Again

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,5 +2,5 @@
 
 Paperclip::Attachment.default_options[:url] = 'variations.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:s3_host_alias] = 'photos.musicales-tannay.ch'
-Paperclip::Attachment.default_options[:path] = '/photos/:style/:filename:origstamp'
+Paperclip::Attachment.default_options[:path] = '/photos/:style/:filename'
 Paperclip::Attachment.default_options[:use_timestamp] = false
