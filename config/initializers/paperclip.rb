c90ff471430025b5c3f75a10063defeb44a45897
Voight-Kampff <valfells@gmail.com>
2013-12-10 22:55:56 -0500

    ignore the last one

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,7 @@
 #options for paperclip
 
 Paperclip::Attachment.default_options[:url] = 's3_alias_url'
-#Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
 Paperclip::Attachment.default_options[:s3_host_alias] = 'photos.musicales-tannay.ch'
 Paperclip::Attachment.default_options[:path] = '/photos/:style/:filename'
 Paperclip::Attachment.default_options[:use_timestamp] = false
