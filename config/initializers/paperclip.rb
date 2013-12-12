0ecf0113798a28d93029b490c98edd869cb88361
Voight-Kampff <valfells@gmail.com>
2013-12-12 16:55:57 -0500

    Removed SSL

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,4 +3,4 @@
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:style/:filename'
 Paperclip::Attachment.default_options[:use_timestamp] = false
-Paperclip::Attachment.default_options[:s3_protocol] = 'https'
+#Paperclip::Attachment.default_options[:s3_protocol] = 'https'
