06c00951972d9571eda007da4734628ba2d9d44c
Voight-Kampff <valfells@gmail.com>
2014-03-11 19:58:39 +0100

    Changed Home Page

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -11,15 +11,23 @@ class Concert < ActiveRecord::Base
   #front: images that will appear on program page
   has_attached_file :front, styles: {
   		thumb: '260>x260'
-  }
+  },
+  :url => ':s3_alias_url',
+  :s3_host_alias => 'photos.musicales-tannay.ch'
 
   #image 1 and 2, that will appear on dedicated page
   has_attached_file :image1, styles: {
   		full: '570>x260'
-  }
+  },
+  :url => ':s3_alias_url',
+  :s3_host_alias => 'photos.musicales-tannay.ch'
+
   has_attached_file :image2, styles: {
   		full: '570>x260'
-  }
+  },
+  :url => ':s3_alias_url',
+  :s3_host_alias => 'photos.musicales-tannay.ch'
+
 
 
   private
