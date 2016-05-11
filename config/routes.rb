a99373eb06fc54be0e7228e8a3372db638f96ab9
Voight-Kampff <valfells@gmail.com>
2016-05-11 09:43:41 +0200

    fixed another error

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 VMT::Application.routes.draw do
 
-
-  
-
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   resources :orders
   resources :ccpayments
   resources :concerts
