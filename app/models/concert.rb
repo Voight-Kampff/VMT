720c5a35e8fe4fcae35f107101b6110bc2864541
Voight-Kampff <valfells@gmail.com>
2016-05-11 09:39:50 +0200

    fixed error

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,12 +1,8 @@
 class Concert < ActiveRecord::Base
-  default_scope  { order('created_at ASC') }
+  default_scope order('date ASC')
   serialize :playlist, Array
   attr_accessible :date, :description, :head, :subhead, :shortname, :playlist, :category
   has_many :artists
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
 
   attr_accessible :image1, :image2, :front #paperclip attributes 
 
