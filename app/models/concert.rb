90f5b5383594549e38563f462938acdd8e11d565
Voight-Kampff <valfells@gmail.com>
2016-05-26 14:26:44 +0200

    wtf

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -6,7 +6,7 @@ class Concert < ActiveRecord::Base
   has_many :tickets
   has_many :seats
 
-  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+  #validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
 
 
   attr_accessible :image1, :image2, :front #paperclip attributes 
