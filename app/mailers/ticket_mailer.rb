4587379d81f2d7cef25c0c12c5ba8ce3f90d6d18
Voight-Kampff <valfells@gmail.com>
2016-05-31 23:03:25 +0200

    Fixed turns out S3 was missing a couple of files

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -15,27 +15,27 @@ class TicketMailer < ActionMailer::Base
         qr_image = MiniMagick::Image.open("#{@order.code}.png")
 
         ticket_image = MiniMagick::Image.open("http://photos.musicales-tannay.ch/tickets/ticket_#{reservation.seat.concert_id}.png")
-      #   column_image = MiniMagick::Image.open("http://photos.musicales-tannay.ch/tickets/#{reservation.seat.column}.png")
-      #   row_image = MiniMagick::Image.open("http://photos.musicales-tannay.ch/tickets/#{reservation.seat.row}.png")
+        column_image = MiniMagick::Image.open("http://photos.musicales-tannay.ch/tickets/#{reservation.seat.column}.png")
+        row_image = MiniMagick::Image.open("http://photos.musicales-tannay.ch/tickets/#{reservation.seat.row}.png")
 
-      #   ticket_with_code = ticket_image.composite(qr_image) do |c|
-      #       c.compose "Over"    # OverCompositeOp
-      #       c.geometry "+500+100" # copy second_image onto first_image from (500, 100)
-      #   end
+        ticket_with_code = ticket_image.composite(qr_image) do |c|
+            c.compose "Over"    # OverCompositeOp
+            c.geometry "+500+100" # copy second_image onto first_image from (500, 100)
+        end
 
-      #   ticket_with_code_and_column = ticket_with_code.composite(column_image) do |c|
-      #       c.compose "Over"    # OverCompositeOp
-      #       c.geometry "+350+310" # copy second_image onto first_image from (500, 100)
-      #   end
+        ticket_with_code_and_column = ticket_with_code.composite(column_image) do |c|
+            c.compose "Over"    # OverCompositeOp
+            c.geometry "+350+310" # copy second_image onto first_image from (500, 100)
+        end
 
-      #   result = ticket_with_code_and_column.composite(row_image) do |c|
-      #       c.compose "Over"    # OverCompositeOp
-      #       c.geometry "+150+310" # copy second_image onto first_image from (500, 100)
-      #   end
+        result = ticket_with_code_and_column.composite(row_image) do |c|
+            c.compose "Over"    # OverCompositeOp
+            c.geometry "+150+310" # copy second_image onto first_image from (500, 100)
+        end
 
         #result.write("reservation_#{reservation.id}.png")
 
-        attachments.inline["reservation_#{reservation.id}.png"] =File.read(ticket_image.tempfile)
+        attachments.inline["reservation_#{reservation.id}.png"] =File.read(result.tempfile)
     end
 
     mail(:to => @order.email, :from => "Billetterie@musicales-tannay.ch", :bcc => "webmaster@musicales-tannay.ch", :subject => "Vos billets pour les variations musicales de Tannay")
