61a423b8c41eece6039bdfbe11d734239f9470e1
Voight-Kampff <valfells@gmail.com>
2016-08-21 13:28:06 +0200

    added paperclip validation

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -8,4 +8,7 @@ class Photo < ActiveRecord::Base
 	},
 	:url => ':s3_alias_url',
     :s3_host_alias => 'photos.musicales-tannay.ch'
+
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+    
 end
